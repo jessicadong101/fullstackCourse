868944713f8b235577fe577bedd8b8a901898571
Jessica Dong <jessicadong101@berkeley.edu>
2022-05-25 10:57:26 -0700

    initial commit

---
+++ part1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  console.log('Hello')
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+export default App;
